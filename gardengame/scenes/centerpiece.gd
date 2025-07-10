5916238916690aa9735ac96b20f5caf8d24cc175
Mingar3x <127351206+Mingar3x@users.noreply.github.com>
2025-07-10 16:00:10 -0400

    big updateeee

--- gardengame/scenes/centerpiece.gd
+++ gardengame/scenes/centerpiece.gd
@@ -1,11 +1,20 @@
 extends Node2D
 @export var health = 5
+var z_part = load("res://scenes/z_particle.tscn")
+var zs = []
 func _ready():
 	await get_tree().process_frame
 	await get_tree().process_frame
 	#haha mojang would never do this
 	GlobalScript.gamemanger.centerpiece = self
-
 func _deal_damage(amt: int):
 	health -= amt
 	print(health)
+	if zs.get(1) == null:
+		var a = null
+		a.kill()
+	else:
+		pass
+		#var dead_guy = zs.get(0)
+		#zs.remove_at(0)
+		#dead_guy.queue_free()
---
+++ gardengame/scenes/circle_enemy.gd
@@ -0,0 +1,3 @@
+extends "res://scenes/enemy.gd"
+func _init():
+	movement_chance = 0.3
